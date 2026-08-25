9b9c7c8c5ffca8fd440500fe60cfdfbf49df7ae9
dlxodnjs2456-hash <dlxodnjs2456@gmail.com>
2026-08-25 18:45:59 +0900

    Add checker API settings

--- app/settings.py
+++ app/settings.py
@@ -8,6 +8,13 @@ class Settings(BaseSettings):
     supabase_service_role_key:str=''
     trongrid_api_key:str=''
     trongrid_base_url:str='https://api.trongrid.io'
+    check_api_base_url:str=''
+    check_api_endpoint:str=''
+    check_api_key:str=''
+    check_api_secret:str=''
+    check_api_auth_type:str='BEARER'
+    check_api_timeout_seconds:int=20
+    check_api_rate_limit_per_minute:int=20
     model_config=SettingsConfigDict(env_prefix='HUB24_',env_file='.env',extra='ignore')
 
 settings=Settings()
