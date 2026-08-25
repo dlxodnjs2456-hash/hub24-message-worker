9ebf7b5104ae541278d2bc97cb2a4bfdb33c22a9
dlxodnjs2456-hash <dlxodnjs2456@gmail.com>
2026-08-25 12:10:04 +0900

    Allow HUB24 Vercel web origins in worker CORS

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     encryption_key:str=''
     session_dir:str='./data/sessions'
-    cors_origins:str='http://localhost:3000'
+    cors_origins:str='http://localhost:3000,https://hub24-message-web-sandy.vercel.app,https://hub24-message-web.vercel.app'
     supabase_url:str=''
     supabase_service_role_key:str=''
     model_config=SettingsConfigDict(env_prefix='HUB24_',env_file='.env',extra='ignore')
