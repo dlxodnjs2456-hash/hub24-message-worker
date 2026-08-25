0481ed701c3f17b689e266148e7ecff5bb1c2843
dlxodnjs2456-hash <dlxodnjs2456@gmail.com>
2026-08-25 15:36:18 +0900

    Add TronGrid settings for USDT autocharge

--- app/settings.py
+++ app/settings.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     cors_origins:str='http://localhost:3000,https://hub24-message-web-sandy.vercel.app,https://hub24-message-web.vercel.app'
     supabase_url:str=''
     supabase_service_role_key:str=''
+    trongrid_api_key:str=''
+    trongrid_base_url:str='https://api.trongrid.io'
     model_config=SettingsConfigDict(env_prefix='HUB24_',env_file='.env',extra='ignore')
 
 settings=Settings()
